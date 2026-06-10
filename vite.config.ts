95b731ed144ec4d1d3fcc7c231e4b084a634cee0
Nikhita R <156603924+nikhita-raj@users.noreply.github.com>
2026-06-10 16:55:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+
 
 export default defineConfig(({ mode }) => ({
   server: {
@@ -11,10 +11,7 @@ export default defineConfig(({ mode }) => ({
     allowedHosts: true,
   },
 
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(),]
 
   resolve: {
     alias: {
